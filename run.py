d9420f262777d9892a2a3da5c81616de37192bf2
Adam Green <adam@bristlebird.com>
2024-07-16 18:06:18 +0000

    add menu index function for timer options

--- run.py
+++ run.py
@@ -16,6 +16,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('time_lord')
 
+"""
+A selection of most common night / cheap rate time windows
+    1: '12am to 9am' - Electric Ireland Summer night rate
+    2: '11pm to 8am' - Electric Ireland winter night rate
+    3: '1am to 8am', - SSE Airtricity winter 
+    4: '2am to 9am', - SSE Airtricity summer 
+    5: '2am to 4am', - EI cheap boost 
+    6: '2am to 5am', — Pinergy EV
+    7: '7pm to 10pm', - Pinergy Family time
+"""
 times = {
     1: ['12am to 9am', '00:00', '09:00'],
     2: ['11pm to 8am', '23:00', '08:00'],
@@ -26,6 +36,15 @@ times = {
     7: ['7pm to 10pm', '19:00', '22:00'],
     8: ['Exit']
 }
+timer_options = {
+    1: ['Start delay (number of hours until appliance starts)'],
+    2: ['End delay (number of hours until appliance is finished)'],
+    3: ['Start time'],
+    4: ['End time'],
+    5: ['None of the above'],
+    6: ['Exit']
+}
+
 appliances = ['Dishwasher', 'Washing machine', 'Bread machine', 'Oven', 'Yogurt maker', 'Other', 'Exit' ]
 
 def print_welcome():
@@ -37,52 +56,10 @@ def print_time_options():
     for key in times.keys():
         print (key, '--', times[key][0] )
 
-def set_times_index():
-    """
-    Input #1 — Get night rate period from the user.
-    Use a menu option to select from preset time windows.
-    Python terminal menu code from: 
-    https://computinglearner.com/how-to-create-a-menu-for-a-python-console-application/
-    Runs a while loop to collect a valid integer from the user
-    via the terminal. The loop will repeatedly request data, until it is valid.
-    Uses int method to check for valid integer.
-
-    1: '12am to 9am' - Electric Ireland Summer night rate
-    2: '11pm to 8am' - Electric Ireland winter night rate
-    3: '1am to 8am', - SSE Airtricity winter 
-    4: '2am to 9am', - SSE Airtricity summer 
-    5: '2am to 4am', - EI cheap boost 
-    6: '2am to 5am', — Pinergy EV
-    7: '7pm to 10pm', - Pinergy Family time
-    8: exit
-    """
-    print("First, tell me the times that your cheapest electricity rates apply. These are examples of the most ")
-    print("common time windows for lower night rate electricity offered by Irish energy providers in 2024.\n")
-    
-    while(True):
-        print("Please choose one of the following low rate time windows:")
-        print_time_options()
-        option = ''
-        try:
-            option = int(input('Enter your choice: '))            
-        except:
-            print('Wrong input. Please enter a number ...')
-        # Check that choice is within range of options
-        if option not in range(1,len(times)+1): 
-            print(f"\nInvalid option. Please enter a number between 1 and {len(times)}.\n")
-        else:
-            # Handle exit
-            if option == 8:
-                print('\nExiting Time Lord... thank you & bye bye!\n')
-                exit()
-            # set time_window
-            return option
-
 def set_appliance(appliance_list):
     """
     Input #2 — Get the name of the appliance from the list of available appliances.
     """
-    
     while(True):
         print("Please choose the appliance that you're setting a timer for:")
         for i in range(len(appliance_list)):
@@ -97,10 +74,37 @@ def set_appliance(appliance_list):
             print(f"\nInvalid option. Please enter a number between 1 and {len(appliance_list)}.\n")
         else:
             # Handle exit
-            if appliance_list[option-1] == 'exit':
+            if appliance_list[option-1].lower() == 'exit':
                 print('\nExiting Time Lord... thank you & bye bye!\n')
                 exit()
             return appliance_list[option-1]
+
+
+def get_menu_index_from(menu_dict):
+    """
+    Allows user to choose an option from a menu dictionary passed as an argument
+    """
+    while(True):
+        print("Please choose one of the following options:")
+        for key in menu_dict.keys():
+            print (key, '--', menu_dict[key][0] )
+
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))            
+        except:
+            print('Wrong input. Please enter a number ...')
+        # Check that choice is within range of options
+        if option not in range(1,len(menu_dict)+1): 
+            print(f"\nInvalid option. Please enter a number between 1 and {len(menu_dict)}.\n")
+        else:
+            # Handle exit
+            if menu_dict[option][0].lower() == 'exit':
+                print('\nExiting Time Lord... thank you & bye bye!\n')
+                exit()
+            # return selected option
+            return option
+
 
 def main():
     """
@@ -109,9 +113,17 @@ def main():
     print_welcome()
     now = datetime.now(pytz.timezone("Europe/Dublin"))
     print("The current time in Ireland is:", now.strftime("%H:%M"))
-    times_index = set_times_index()
-    print(f"Selected low rate time window: {times[times_index][0]}\n")
+
+    print("\nFirst, tell me the times that your cheapest electricity rates apply. These are examples of the most ")
+    print("common time windows for lower night rate electricity offered by Irish energy providers in 2024.\n")
+    times_index = get_menu_index_from(times)
+    print(f"\nSelected low rate time window: {times[times_index][0]}\n")
+
+    # print("Next, select which appliance you're setting a timer for.\n")
     appliance = set_appliance( appliances )
-    print(f"Appliance chosen: {appliance}")
+    print(f"\nAppliance chosen: {appliance}\n")
+    print(f"Now, tell me which of the following best describes the input your {appliance.lower()} requires to set the time delay.\n")
+    timer_index = get_menu_index_from(timer_options)
+    print(f"\nSelected timer option: {timer_options[timer_index][0]}\n")
 
 main()
